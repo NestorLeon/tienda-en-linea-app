0a45025e040fc128f8ace957c4e56a7bd75ac975
Juan Carlos González Mendoza <87685467+juancarlosgm1997@users.noreply.github.com>
2023-05-19 12:31:41 -0600

    commit inicial del Sprint 1 (#1)
    Aprobado PR #1, se creó rama PBI_22
    Co-authored-by: Néstor León <nessu2@gmail.com>

---
+++ resources/views/tienda/show_producto.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Como cliente Ver Producto de la Tienda</title>
+</head>
+<body>
+    <h1>Como cliente Ver Producto de la Tienda</h1>
+</body>
+</html>
